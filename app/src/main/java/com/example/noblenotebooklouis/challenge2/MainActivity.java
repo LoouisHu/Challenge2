5aa01fe5acfda4ac1f70b07bda859924f3fd42f6
s1530003 <l.hu@student.utwente.nl>
2017-09-13 09:58:22 +0200

    initial commit

---
+++ app/src/main/java/com/example/noblenotebooklouis/challenge2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.noblenotebooklouis.challenge2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.estimote.coresdk.cloud.api.EstimoteCloud;
+import com.estimote.coresdk.common.config.EstimoteSDK;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

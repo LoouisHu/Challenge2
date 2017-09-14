b714004a610c63786fc8ddc515dcc168becc0568
Thomas <thomasvrielink92@gmail.com>
2017-09-14 09:54:32 +0200

    app nog zonder functionaliteit gemaakt

--- app/src/main/java/com/example/noblenotebooklouis/challenge2/MainActivity.java
+++ app/src/main/java/com/example/noblenotebooklouis/challenge2/MainActivity.java
@@ -1,15 +1,79 @@
 package com.example.noblenotebooklouis.challenge2;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewGroup mainLayout;
+    private ImageView image;
 
+    private int xDelta;
+    private int yDelta;
+
+    private BluetoothAdapter adapter;
+    private BluetoothLeScanner scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = BluetoothAdapter.getDefaultAdapter();
+
+        scanner = adapter.getBluetoothLeScanner();
+
+        image = (ImageView) findViewById(R.id.image);
+
+        image.setOnTouchListener(onTouchListener());
     }
+
+    /**private OnTouchListener onTouchListener() {
+        return new OnTouchListener() {
+
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                final int x = (int) event.getRawX();
+                final int y = (int) event.getRawY();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                view.getLayoutParams();
+
+                        xDelta = x - lParams.leftMargin;
+                        yDelta = y - lParams.topMargin;
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                                .getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.topMargin = y - yDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                mainLayout.invalidate();
+                return true;
+            }
+        };
+    }*/
 }
